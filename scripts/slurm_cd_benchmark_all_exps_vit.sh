8a0a3703b496cee64c36a7523ea3d5ffeade42a0
Brandon <brandon.lee@ea.epson.com>
2026-07-04 22:24:47 -0400

    drop all the exp7 extensions

--- scripts/slurm_cd_benchmark_all_exps_vit.sh
+++ scripts/slurm_cd_benchmark_all_exps_vit.sh
@@ -7,15 +7,15 @@
 #SBATCH --mem=16G
 #SBATCH --gpus-per-node=1
 #SBATCH --exclude=node1
-#SBATCH --time=5:00:00
-#SBATCH --array=0-111
+#SBATCH --time=4:00:00
+#SBATCH --array=0-55
 #SBATCH --output=/share_98/projects/brandon/repos/pta/logs/all_exps_vit_%x-%A_%a.out
 #SBATCH --error=/share_98/projects/brandon/repos/pta/logs/all_exps_vit_%x-%A_%a.err
 
 # ============================================================================
 # Slurm array job: All CD benchmark experiments (ViT-B/16)
 #
-# 112 tasks = 16 experiments × 7 CD core datasets
+# 56 tasks = 8 experiments × 7 CD core datasets
 #
 #   exp_idx = SLURM_ARRAY_TASK_ID / 7
 #   ds_idx  = SLURM_ARRAY_TASK_ID % 7
@@ -30,14 +30,6 @@
 #    5  Exp10SoftPatchGate              exp10_soft_patch_gate           configs_exp10
 #    6  Exp11TunableFusionNew           exp11_tunable_fusion_new        configs_exp11
 #    7  Exp12PatchQualityModulation     exp12_patch_quality_modulation  configs_exp12
-#    8  Exp7a-TauRange50_100            exp7_inverted_image_weight      configs_exp7a
-#    9  Exp7b-TauRange50_200            exp7_inverted_image_weight      configs_exp7b
-#   10  Exp7c-TauRange0_100             exp7_inverted_image_weight      configs_exp7c
-#   11  Exp7d-SqrtMapping              exp7d_inverted_image_weight_sqrt  configs_exp7d
-#   12  Exp7e-SquareMapping            exp7e_inverted_image_weight_square configs_exp7e
-#   13  Exp7f-LowPatchProto             exp7_inverted_image_weight      configs_exp7f
-#   14  Exp7g-HighPatchProto            exp7_inverted_image_weight      configs_exp7g
-#   15  Exp7h-LowTextWeight             exp7_inverted_image_weight      configs_exp7h
 #
 # CD core datasets (all same 7 for every experiment):
 #   caltech101 dtd eurosat fgvc oxford_flowers oxford_pets ucf101
@@ -75,15 +67,6 @@ METHODS=(
     exp10_soft_patch_gate
     exp11_tunable_fusion_new
     exp12_patch_quality_modulation
-    # Exp7 extensions
-    exp7_inverted_image_weight           # Exp7a
-    exp7_inverted_image_weight           # Exp7b
-    exp7_inverted_image_weight           # Exp7c
-    exp7d_inverted_image_weight_sqrt     # Exp7d
-    exp7e_inverted_image_weight_square   # Exp7e
-    exp7_inverted_image_weight           # Exp7f
-    exp7_inverted_image_weight           # Exp7g
-    exp7_inverted_image_weight           # Exp7h
 )
 
 CONFIG_DIRS=(
@@ -95,15 +78,6 @@ CONFIG_DIRS=(
     configs_exp10
     configs_exp11
     configs_exp12
-    # Exp7 extensions
-    configs_exp7a
-    configs_exp7b
-    configs_exp7c
-    configs_exp7d
-    configs_exp7e
-    configs_exp7f
-    configs_exp7g
-    configs_exp7h
 )
 
 EXP_LABELS=(
@@ -115,15 +89,6 @@ EXP_LABELS=(
     "Exp10-SoftPatchGate"
     "Exp11-TunableFusionNew"
     "Exp12-PatchQualityModulation"
-    # Exp7 extensions
-    "Exp7a-TauRange50-100"
-    "Exp7b-TauRange50-200"
-    "Exp7c-TauRange0-100"
-    "Exp7d-SqrtMapping"
-    "Exp7e-SquareMapping"
-    "Exp7f-LowPatchProto"
-    "Exp7g-HighPatchProto"
-    "Exp7h-LowTextWeight"
 )
 
 # ---------------------------------------------------------------------------
